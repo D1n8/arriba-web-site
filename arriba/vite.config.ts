b8bf7e9f44835c396034c46c1d7644686431f6e4
D1n8 <mr.dinyar@mail.ru>
2025-01-13 14:34:57 +0500

    update

--- arriba/vite.config.ts
+++ arriba/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://optovolokn0.github.io/arriba/arriba"
+  base: "https://d1n8.github.io/arriba-web-site/arriba"
 })
